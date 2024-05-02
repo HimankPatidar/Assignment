ed8c39ba922fffdc7628397516f641914585f709
Himank Patidar <patidarhimank005@gmail.com>
2024-05-03 00:25:42 +0530

    completed

--- assignment10/reciever.py
+++ assignment10/reciever.py
@@ -8,7 +8,6 @@ def receive_file(save_path, local_ip, local_port):
     s.bind(complete_address)
     print("Waiting for file...")
 
-    # Receive the start signal to begin file reception
     data, addr = s.recvfrom(1024)
     if data == b'START_FILE_TRANSMISSION':
         with open(save_path, "wb") as file:
@@ -19,7 +18,6 @@ def receive_file(save_path, local_ip, local_port):
                 file.write(data)
         print("File received successfully.")
 
-# Example usage
 save_path = "./recieved/1.pdf"
 local_ip = "192.168.1.55"
 local_port = 4343
